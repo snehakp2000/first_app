359f61f5c90cc66a3873fde3be145e30f23af104
snehakp2000 <snehakallullaparambu@gmail.com>
2025-08-20 15:43:53 +0530

    created another class

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.purple, Colors.yellow],
-              begin: Alignment.topRight,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.purple, Colors.yellow],
+          begin: Alignment.topRight,
+          end: Alignment.bottomRight,
         ),
       ),
-    ),
-  );
+      child: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
