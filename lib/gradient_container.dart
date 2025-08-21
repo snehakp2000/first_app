69ce241785d804399ecb78d4b186115b265832ca
snehakp2000 <snehakallullaparambu@gmail.com>
2025-08-21 15:06:20 +0530

    more fomatting to the file structure

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,24 +1,21 @@
+import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
+  const GradientContainer(this.colors1, this.colors2, {super.key});
+  final Color colors1;
+  final Color colors2;
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.purple, Colors.yellow],
+          colors: [colors1,colors2],
           begin: Alignment.topRight,
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(color: Colors.white, fontSize: 28),
-        ),
-      ),
+      child: const Center(child: StyledText("hello world")),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,9 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: GradientContainer(Colors.purple, Colors.yellow)),
+    ),
+  );
 }
